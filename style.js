46a1970031bdd758ab240ca56766d70998511ece
guillermojh <ing.guillermoh@gmail.com>
2023-04-16 22:11:00 -0300

    1.0

---
+++ style.js
@@ -0,0 +1,23 @@
+// const nav = document.querySelector(".nav");
+
+// window.addEventListener("scroll", fixNav);
+
+// // let titulo = document.querySelector("title").innerText;
+// let titulo = document.title;
+
+// function fixNav() {
+//   if (window.scrollY > nav.offsetHeight + 150) {
+//     nav.classList.add("active");
+//   } else {
+//     nav.classList.remove("active");
+//   }
+// }
+
+// window.onblur = function () {
+//   // Si el usuario se va a otro lado...
+//   document.title = "Hey, vuelve aquí"; // Cambio el título
+// };
+
+// window.onfocus = function () {
+//   document.title = titulo; // Si vuelvo, restablezco el título
+// };
